8321af35b0c9751a62c996db153639131f2e5397
SujanCh564 <132272276+SujanCh564@users.noreply.github.com>
2023-08-18 08:16:36 +0545

    Update utils.py

--- utils.py
+++ utils.py
@@ -730,7 +730,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                         reply_markup=InlineKeyboardMarkup(
                             [
                                 [
-                                l           InlineKeyboardButton('Bᴏᴛ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
+                                l           InlineKeyboardButton("Bᴏᴛ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=CHNL_LNK)
                             ],[
                                 InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Sujan_Ch")
                                 ]
