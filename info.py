cd7fff5526d88dc2843e78e843807807562d6ac7
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-08-18 15:05:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,6 +99,6 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 #Direct Link Generator By @AB_BotZ_Update
-DIRECT_GEN_DB = int(environ.get("DIRECT_GEN_DB", "-1001731526413")) # Enter your channel id
+DIRECT_GEN_DB = int(environ.get("DIRECT_GEN_DB", "-1001230076027")) # Enter your channel id
 DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://rockersdisc.blogspot.com") # https://example.com/
 DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)
