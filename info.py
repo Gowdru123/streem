6d9143dc6544d264abc4be9998646a19208457bf
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-08-18 15:17:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,5 +100,5 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 #Direct Link Generator By @AB_BotZ_Update
 DIRECT_GEN_DB = int(environ.get("DIRECT_GEN_DB", "-1001230076027")) # Enter your channel id
-DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://spaceb.in/jlpOugPV") # https://example.com/
+DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://hanghogaya.systems/watch/") # https://example.com/
 DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)
